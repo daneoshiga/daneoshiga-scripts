9e3723266d12aba79805dd3bfb6f8ea51db1e711
Danilo Shiga <daniloshiga@gmail.com>
2012-06-29 23:07:11 -0300

    adding gpodownload script

--- README.markdown
+++ README.markdown
@@ -1,30 +1,30 @@
 I have here some small bash scripts that I've made, or changed from others
 
 conky_startup.sh
-=
+-
 
 just a wrapper for conky, to delay it's start on boot (which makes it start
 above X)
 
 limitador.sh
-=
+-
 
 limit the speed of rtorrent and jdownloader when nobody is using the pc,
 uses xprintidle to check if there's any user activity
 
 mplayer
-=
+-
 
 implements "bookmarks" on mplayer, if any bookmark is set, the video will
 continue from that point when played again
 
 playrar.sh
-=
+-
 
 plays video files inside rar files.
 
 mashpodder.sh
-=
+-
 
 my fork of mashpodder, the shell script podcast downloader, i've added:
 speed limit config for wget
@@ -34,3 +34,8 @@ mp.conf, not only for the number of new downloads on that execution.
 added the other needed files to mashpodder
 mashpodder original repository
 http://code.google.com/p/mashpodder/
+
+gpodownload.sh
+-
+
+an small script to download videos from specific channels on gpodder
---
+++ gpodownload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gpo update "http://gdata.youtube.com/feeds/base/users/CHOW/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile"
+gpo download "http://gdata.youtube.com/feeds/base/users/CHOW/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile"
+gpo download "http://gdata.youtube.com/feeds/base/users/kincommunity/uploads?orderby=updated&alt=rss&client=ytapi-youtube-rss-redirect&v=2"
+gpo download "http://gdata.youtube.com/feeds/base/users/modernmom/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile"
+gpo update "http://gdata.youtube.com/feeds/base/users/TheOfficialHungry/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile"
+gpo download "http://gdata.youtube.com/feeds/base/users/TheOfficialHungry/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile"
+gpo update   "http://gdata.youtube.com/feeds/base/users/trailers/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile"
+gpo download "http://gdata.youtube.com/feeds/base/users/trailers/uploads?alt=rss&v=2&orderby=published&client=ytapi-youtube-profile"
